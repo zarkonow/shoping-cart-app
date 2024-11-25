47bf140603fdbfcd44b6ce04f9494957514704b1
zarkonow <stojanov1987@gmail.com>
2024-11-25 23:58:32 +0100

    *added slice

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -35,14 +35,15 @@ export default function Home() {
           />
         </div>
       ) : (
-        <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5
-         space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
-            {
-                products && products.length ? 
-                products.map(productItem => <ProductTile key={productItem.id} product={productItem}/>)
-                :null
-            }
-         </div>
+        <div
+          className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3"
+        >
+          {products && products.length
+            ? products.map((productItem) => (
+                <ProductTile key={productItem.id} product={productItem} />
+              ))
+            : null}
+        </div>
       )}
     </div>
   );
